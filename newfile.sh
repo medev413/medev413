74275d1c459c59ab8f4cc31046c1d0268f587175
Devalam <dbhargav.mac@gmail.com>
2024-10-09 14:29:33 +0000

    Modified newfile

--- newfile.sh
+++ newfile.sh
@@ -3,3 +3,12 @@
 name=”Jayesh”
 age=21
 echo $name $age
+
+
+
+#!/bin/bash
+
+# example of using arguments to a script
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#"
